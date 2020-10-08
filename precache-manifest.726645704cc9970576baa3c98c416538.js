b0124085bd846da87bc0ea71e424c1a332bbe6c8
Arnav Srivastava <srivarnav@gmail.com>
2020-10-08 17:26:32 +0530

    Updates

---
+++ precache-manifest.726645704cc9970576baa3c98c416538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35aa0a1212c7d7fb9df74b40a5b0460",
+    "url": "/TaxBlock/index.html"
+  },
+  {
+    "revision": "c33968fc9da332f4aa91",
+    "url": "/TaxBlock/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f79c789fa48c4b09a29",
+    "url": "/TaxBlock/static/js/2.5d25499d.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/TaxBlock/static/js/2.5d25499d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33968fc9da332f4aa91",
+    "url": "/TaxBlock/static/js/main.1706c45c.chunk.js"
+  },
+  {
+    "revision": "6455761fa9d58a7b1c67",
+    "url": "/TaxBlock/static/js/runtime-main.d35ea18a.js"
+  }
+]);
